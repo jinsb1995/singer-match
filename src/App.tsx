325fb3b65733755712cda4cbf2e274a1700a718b
jinsb1995 <jinsb1995@gmail.com>
2021-06-28 23:59:31 +0900

    메인화면 레이아웃 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,100 @@
-import { Input,Table,Space,Tag } from 'antd';
-import logo from './logo.svg';
-import TestButton from './Common/Buttons/index'
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+import './index.css';
 
-function App() {
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      render: (text:any) => <a>{text}</a>,
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-      render: (tags:any) => (
-        <>
-          {tags.map((tag:any) => {
-            let color = tag.length > 5 ? 'geekblue' : 'green';
-            if (tag === 'loser') {
-              color = 'volcano';
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </>
-      ),
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text:any, record:any) => (
-        <Space size="middle">
-          <a>Invite {record.name}</a>
-          <a>Delete</a>
-        </Space>
-      ),
-    },
-  ];
-  
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-    },
-  ];
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const App = () => {
 
 
-  return (
-    <>
-    <Table columns={columns} dataSource={data} />
-      <TestButton name={"asasdasdd"}/>
-    </>
-  );
+    return (
+        <Layout>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+            >
+                <div className="logo"> 
+                  로고
+                </div>
+                <div style={{ backgroundColor: 'white', width: '70%', margin: '30px auto' }}> 
+                  로그인 정보(css 수정)
+                </div>
+
+                <hr />
+
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                    <Menu.Item key="1" icon={<UserOutlined />}>
+                        nav 1
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                        nav 2
+                    </Menu.Item>
+                    <Menu.Item key="3" icon={<UploadOutlined />}>
+                        nav 3
+                    </Menu.Item>
+                    <Menu.Item key="4" icon={<UserOutlined />}>
+                        nav 4
+                    </Menu.Item>
+                    <Menu.Item key="5" icon={<UserOutlined />}>
+                        nav 5
+                    </Menu.Item>
+                    <Menu.Item key="6" icon={<UserOutlined />}>
+                        nav 6
+                    </Menu.Item>
+                    <Menu.Item key="7" icon={<UserOutlined />}>
+                        nav 7
+                    </Menu.Item>
+                    <Menu.Item key="8" icon={<UserOutlined />}>
+                        nav 8
+                    </Menu.Item>
+                </Menu>
+
+                <hr />
+
+                <div style={{ marginTop: '30px' }}>
+                  <label style={{ color: 'white' }}>보관함 같은것들</label>
+                  <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                      <Menu.Item key="1" icon={<UserOutlined />}>
+                          nav 1
+                      </Menu.Item>
+                      <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                          nav 2
+                      </Menu.Item>
+                      <Menu.Item key="3" icon={<UploadOutlined />}>
+                          nav 3
+                      </Menu.Item>
+                  </Menu>
+                </div>
+            </Sider>
+
+            
+            
+            <Layout>
+                <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+                <Content style={{ margin: '24px 16px 0', overflowY: 'scroll' }}>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 300, marginBottom: '10px'}}>
+                        content
+                    </div>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 300, marginBottom: '10px' }}>
+                        content
+                    </div>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 300, marginBottom: '10px' }}>
+                        content
+                    </div>
+                    {/* <Test /> */}
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
+    );
 }
 
 export default App;
